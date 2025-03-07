a28ab1eda23bf3f679ef9c6638317cb875d82bb0
jsw <jsw@hanuriit.co.kr>
2025-03-07 20:00:02 +0900

    JPA 멀티 페치 조인 프로젝트 생성

---
+++ jpa-multi-entity/src/main/java/io/philz/jpa_multi_entity/JpaMultiEntityApplication.java
@@ -0,0 +1,13 @@
+package io.philz.jpa_multi_entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMultiEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaMultiEntityApplication.class, args);
+	}
+
+}
---
+++ jpa-multi-entity/src/test/java/io/philz/jpa_multi_entity/JpaMultiEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.philz.jpa_multi_entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMultiEntityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
