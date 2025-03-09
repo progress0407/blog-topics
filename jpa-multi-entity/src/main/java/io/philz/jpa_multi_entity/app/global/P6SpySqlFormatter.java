8e298e83abb4d3271e9f321b7e26bc1b66f57b50
progress0407 <progress0407@gmail.com>
2025-03-09 15:37:35 +0900

    1. p6spy 작성  2. gradle 캐시 처리

---
+++ jpa-multi-entity/src/main/java/io/philz/jpa_multi_entity/app/global/P6SpySqlFormatter.java
@@ -0,0 +1,58 @@
+package io.philz.jpa_multi_entity.app.global;
+
+import static org.springframework.util.StringUtils.*;
+
+import java.util.Locale;
+
+import org.hibernate.engine.jdbc.internal.FormatStyle;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.p6spy.engine.logging.Category;
+import com.p6spy.engine.spy.P6SpyOptions;
+import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
+
+import jakarta.annotation.PostConstruct;
+
+@Profile({"default", "local"})
+@Configuration
+public class P6SpySqlFormatter implements MessageFormattingStrategy {
+
+    @PostConstruct
+    public void setLogMessageFormat() {
+        P6SpyOptions.getActiveInstance().setLogMessageFormat(this.getClass().getName());
+    }
+
+    @Override
+    public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql, String url) {
+        String formattedSql = formatSql(category, sql);
+        return formatLog(elapsed, category, formattedSql);
+    }
+
+    private String formatSql(String category, String sql) {
+        if (hasText(sql) && isStatement(category)) {
+            String trimmedSQL = trim(sql);
+            if (isDdl(trimmedSQL)) {
+                return FormatStyle.DDL.getFormatter().format(sql);
+            }
+            return FormatStyle.BASIC.getFormatter().format(sql); // maybe DML
+        }
+        return sql;
+    }
+
+    private static boolean isDdl(String trimmedSQL) {
+        return trimmedSQL.startsWith("create") || trimmedSQL.startsWith("alter") || trimmedSQL.startsWith("comment");
+    }
+
+    private static String trim(String sql) {
+        return sql.trim().toLowerCase(Locale.ROOT);
+    }
+
+    private static boolean isStatement(String category) {
+        return Category.STATEMENT.getName().equals(category);
+    }
+
+    private String formatLog(long elapsed, String category, String formattedSql) {
+        return String.format("[%s] | %d ms | %s", category, elapsed, formatSql(category, formattedSql));
+    }
+}
