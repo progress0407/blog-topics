420b213acc9dfce8ab9bcf38088e1f76cff288ec
progress0407 <progress0407@gmail.com>
2025-03-09 15:25:10 +0900

    1. build.gradle 리펙터링  2. QueryDsl 설정 추가

---
+++ jpa-multi-entity/src/main/java/io/philz/jpa_multi_entity/app/global/QueryDslConfig.java
@@ -0,0 +1,21 @@
+package io.philz.jpa_multi_entity.app.global;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class QueryDslConfig {
+
+    private final EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
